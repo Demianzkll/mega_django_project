de4c88ef99719634edda232dfe6115cd49283988
Demian <demanzakaluzhnij@gmail.com>
2025-11-28 12:15:33 +0200

    improved version

--- maze_app/urls.py
+++ maze_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:size>/', views.maze),
+    path('<int:size>/', views.maze, {'solve': False}, name='maze'),
+    path('<int:size>/solve/', views.maze, {'solve': True}, name='maze_solve'),
+    path('<int:size>/json/', views.maze_json, name='maze_json'),
 ]
--- maze_app/views.py
+++ maze_app/views.py
@@ -7,11 +7,77 @@ from .maze import Maze
 
 
 
-def maze(request, size):
+# def maze(request, size):
+#     m = Maze(size)
+#     m.generate()
+#     m.solve()
+#     # Convert path tuples to string keys so templates can test membership
+#     # e.g. (2,3) -> "2,3"
+#     path_keys = {f"{r},{c}" for (r, c) in m.path}
+#     return render(request, 'maze_app/maze.html', {'grid': m.grid, 'path': path_keys})
+
+
+# def maze(request, size, solve=False):
+#     m = Maze(size)
+#     m.generate()
+
+#     path_keys = set()
+
+#     if solve:
+#         m.solve()
+#         # Convert path tuples to string keys so templates can test membership
+#         path_keys = {f"{r},{c}" for (r, c) in m.path}
+
+#     return render(
+#         request,
+#         'maze_app/maze.html',
+#         {
+#             'grid': m.grid,
+#             'path': path_keys,
+#         }
+#     )
+
+
+
+from django.shortcuts import render
+from django.http import JsonResponse
+from .maze import Maze
+import json
+
+
+def maze(request, size, solve=False):
     m = Maze(size)
     m.generate()
-    m.solve()
-    # Convert path tuples to string keys so templates can test membership
-    # e.g. (2,3) -> "2,3"
-    path_keys = {f"{r},{c}" for (r, c) in m.path}
-    return render(request, 'maze_app/maze.html', {'grid': m.grid, 'path': path_keys})
+
+    if solve:
+        m.solve()
+
+    # у шаблон для JS
+    path = m.path if solve else []
+    visited_order = m.visited_order if solve else []
+
+    return render(
+        request,
+        'maze_app/maze.html',
+        {
+            'grid': m.grid,
+            'size': size,
+            'solve': solve,
+            'path_json': json.dumps(path),
+            'visited_json': json.dumps(visited_order),
+        }
+    )
+
+
+def maze_json(request, size):
+    """Експорт лабіринту в JSON."""
+    m = Maze(size)
+    m.generate()
+    m.solve() 
+
+    data = {
+        'size': size,
+        'grid': m.grid,
+        'path': m.path,
+    }
+    return JsonResponse(data)
